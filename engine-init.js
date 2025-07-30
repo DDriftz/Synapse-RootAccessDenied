b5c4758e0d4df1e9f86e849c0a645fa72ca7ef1b
DDriftz <Hussein86.jobb@gmail.com>
2025-07-30 04:36:35 +0200

    fixes

--- engine-init.js
+++
@@ -1,7 +0,0 @@
-// engine-init.js - Instantiates the GameEngine and attaches to window
-import '../js/core/gameEngine.js';
-
-// If not already present, create the engine and attach to window
-if (!window.engine && window.GameEngine) {
-    window.engine = new window.GameEngine();
-}
